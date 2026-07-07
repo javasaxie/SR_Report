2e00d526e18e1f7c94f3936c7c6e43acd9601ba0
Javas_Axie <javasaxie@gmail.com>
2026-07-07 19:27:22 +0700

    Update purchasing.js

--- js/purchasing.js
+++ js/purchasing.js
@@ -1,627 +1,200 @@
 <script>
 
-
 let purchaseData=[];
-
 let masterItem=[];
-
 let modal;
-
-
-
-
-// =========================
-// INIT
-// =========================
-
 
 function loadPurchasing(){
 
+  modal = new bootstrap.Modal(document.getElementById("purchaseModal"));
 
-modal =
-new bootstrap.Modal(
-document.getElementById(
-"purchaseModal"
-));
-
-
-loadTable();
-
-loadMaster();
-
-
+  loadTable();
+  loadMaster();
 
 }
-
-
-
-
-
-// =========================
-// TABLE
-// =========================
-
 
 function loadTable(){
 
+  google.script.run
 
-google.script.run
+  .withSuccessHandler(function(data){
 
-.withSuccessHandler(function(data){
+    purchaseData=data;
+    renderTable(data);
 
+  })
 
-purchaseData=data;
-
-
-renderTable(data);
-
-
-})
-
-.getPurchasing();
-
-
+  .getPurchasing();
 
 }
-
-
-
 
 function renderTable(data){
 
+  let html="";
 
-let html="";
+  data.forEach(x=>{
 
+    html+=`
+    <tr>
+    <td>${x.date}</td>
+    <td>${x.item}</td>
+    <td>${x.part}</td>
+    <td>${x.merk}</td>
+    <td>${x.vendor}</td>
+    <td>${x.qty}</td>
+    <td>${formatMoney(x.total)}</td>
+    <td><button class="btn btn-danger btn-sm" onclick="deletePurchase('${x.id}')"><i class="bi bi-trash"></i></button></td>
+    </tr>
+    `;
 
-data.forEach(x=>{
+  });
 
-
-html+=`
-
-<tr>
-
-<td>${x.date}</td>
-
-<td>${x.item}</td>
-
-<td>${x.part}</td>
-
-<td>${x.merk}</td>
-
-<td>${x.vendor}</td>
-
-<td>${x.qty}</td>
-
-<td>
-${formatMoney(x.total)}
-</td>
-
-
-<td>
-
-
-<button 
-class="btn btn-danger btn-sm"
-onclick="deletePurchase('${x.id}')">
-
-<i class="bi bi-trash"></i>
-
-</button>
-
-
-</td>
-
-
-</tr>
-
-
-`;
-
-
-
-});
-
-
-document
-.getElementById(
-"purchaseTable"
-)
-.innerHTML=html;
-
-
+  document.getElementById("purchaseTable").innerHTML=html;
 
 }
-
-
-
-
-
-
-// =========================
-// MASTER DATA
-// =========================
-
 
 function loadMaster(){
 
+  google.script.run
 
+  .withSuccessHandler(function(data){
 
-google.script.run
+    masterItem=data;
+    let html=`<option value="">Pilih Item</option>`;
 
-.withSuccessHandler(function(data){
+    data.forEach(x=>{
+      html+=`<option>${x.item}</option>`;
+    });
 
+    document.getElementById("p_item").innerHTML=html;
 
-masterItem=data;
+  })
 
+  .getMasterItem();
 
-let html=
-`<option value="">
-Pilih Item
-</option>`;
+  google.script.run
 
+  .withSuccessHandler(function(data){
 
+    let html="";
+    data.forEach(x=>{
+      html+=`<option>${x.nama}</option>`;
+    });
+    document.getElementById("p_vendor").innerHTML=html;
 
-data.forEach(x=>{
+  })
 
+  .getVendor();
 
-html+=`
+  google.script.run
 
-<option>
-${x.item}
-</option>
+  .withSuccessHandler(function(data){
 
-`;
+    let html="";
+    data.forEach(x=>{
+      html+=`<option>${x}</option>`;
+    });
+    document.getElementById("p_merk").innerHTML=html;
 
+  })
 
-});
-
-
-document
-.getElementById(
-"p_item"
-)
-.innerHTML=html;
-
-
-
-})
-
-.getMasterItem();
-
-
-
-
-
-// Vendor
-
-
-google.script.run
-
-.withSuccessHandler(function(data){
-
-
-let html="";
-
-
-data.forEach(x=>{
-
-
-html+=`
-
-<option>
-${x.nama}
-</option>
-
-
-`;
-
-});
-
-
-document
-.getElementById(
-"p_vendor"
-)
-.innerHTML=html;
-
-
-
-})
-
-.getVendor();
-
-
-
-
-
-// Merk
-
-
-google.script.run
-
-.withSuccessHandler(function(data){
-
-
-let html="";
-
-
-data.forEach(x=>{
-
-
-html+=`
-
-<option>
-${x}
-</option>
-
-
-`;
-
-});
-
-
-document
-.getElementById(
-"p_merk"
-)
-.innerHTML=html;
-
-
-
-})
-
-.getMerk();
-
-
+  .getMerk();
 
 }
-
-
-
-
-
-
-
-
-// =========================
-// ITEM AUTO COMPLETE
-// =========================
-
-
 
 function selectItem(){
 
+  let val = document.getElementById("p_item").value;
+  let item = masterItem.find(x=>x.item==val);
 
-let val =
-document
-.getElementById(
-"p_item"
-)
-.value;
-
-
-
-let item =
-masterItem.find(
-x=>x.item==val
-);
-
-
-
-if(item){
-
-
-document
-.getElementById(
-"p_part"
-)
-.value=item.part;
-
-
+  if(item){
+    document.getElementById("p_part").value=item.part;
+  }
 
 }
-
-
-
-}
-
-
-
-
-
-// =========================
-// MODAL
-// =========================
-
 
 function openPurchaseModal(){
 
-
-document
-.querySelectorAll(
-"#purchaseModal input"
-)
-.forEach(x=>x.value="");
-
-
-modal.show();
-
+  document.querySelectorAll("#purchaseModal input").forEach(x=>x.value="");
+  modal.show();
 
 }
-
-
-
-
-
-// =========================
-// TOTAL
-// =========================
-
 
 function calculateTotal(){
 
-
-let qty =
-Number(
-document.getElementById(
-"p_qty"
-).value);
-
-
-
-let price =
-Number(
-document.getElementById(
-"p_price"
-).value);
-
-
-
-document
-.getElementById(
-"p_total"
-)
-.value=
-
-qty*price;
-
-
+  let qty = Number(document.getElementById("p_qty").value);
+  let price = Number(document.getElementById("p_price").value);
+  document.getElementById("p_total").value = qty*price;
 
 }
-
-
-
-
-
-
-// =========================
-// SAVE
-// =========================
-
 
 function savePurchase(){
 
+  let data={
+    date: p_date.value,
+    item: p_item.value,
+    part: p_part.value,
+    merk: p_merk.value,
+    unit: p_unit.value,
+    vendor: p_vendor.value,
+    satuan: p_satuan.value,
+    qty: p_qty.value,
+    price: p_price.value,
+    inv: p_inv.value,
+    pic: p_pic.value,
+    job: p_job.value
+  };
 
+  google.script.run
 
-let data={
+  .withSuccessHandler(function(){
 
+    Swal.fire("Success", "Data tersimpan", "success");
+    modal.hide();
+    loadTable();
 
+  })
 
-date:
-p_date.value,
-
-
-item:
-p_item.value,
-
-
-part:
-p_part.value,
-
-
-merk:
-p_merk.value,
-
-
-unit:
-p_unit.value,
-
-
-vendor:
-p_vendor.value,
-
-
-satuan:
-p_satuan.value,
-
-
-qty:
-p_qty.value,
-
-
-price:
-p_price.value,
-
-
-inv:
-p_inv.value,
-
-
-pic:
-p_pic.value,
-
-
-job:
-p_job.value
-
-
-
-};
-
-
-
-
-google.script.run
-
-.withSuccessHandler(function(){
-
-
-Swal.fire(
-
-"Success",
-
-"Data tersimpan",
-
-"success"
-
-);
-
-
-modal.hide();
-
-
-loadTable();
-
-
-
-})
-
-
-.savePurchasing(data);
-
-
-
+  .savePurchasing(data);
 
 }
-
-
-
-
-
-
-
-
-// =========================
-// DELETE
-// =========================
-
-
 
 function deletePurchase(id){
 
-
-Swal.fire({
-
-title:
-"Hapus data?",
-
-
-showCancelButton:true,
-
-
-confirmButtonText:
-"Ya"
-
-
-})
-
-
-.then(r=>{
-
-
-if(r.isConfirmed){
-
-
-
-google.script.run
-
-.withSuccessHandler(function(){
-
-
-loadTable();
-
-
-})
-
-.deletePurchasing(id);
-
-
+  Swal.fire({
+    title: "Hapus data?",
+    showCancelButton: true,
+    confirmButtonText: "Ya"
+  })
+  .then(r=>{
+    if(r.isConfirmed){
+      google.script.run
+      .withSuccessHandler(function(){
+        loadTable();
+      })
+      .deletePurchasing(id);
+    }
+  });
 
 }
-
-
-});
-
-
-
-}
-
-
-
-
-
-
-
-
-// SEARCH
-
 
 function filterPurchase(){
 
-
-let key =
-searchPurchase.value
-.toLowerCase();
-
-
-
-let result =
-purchaseData.filter(x=>
-
-
-JSON.stringify(x)
-.toLowerCase()
-.includes(key)
-
-
-);
-
-
-
-renderTable(result);
-
+  let key = searchPurchase.value.toLowerCase();
+  let result = purchaseData.filter(x=> JSON.stringify(x).toLowerCase().includes(key));
+  renderTable(result);
 
 }
-
-
-
-
 
 function formatMoney(v){
 
-
-return new Intl.NumberFormat(
-"id-ID",
-{
-
-style:"currency",
-
-currency:"IDR"
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR"
+  }).format(v);
 
 }
-
-).format(v);
-
-
-
-}
-
-
-
-
 
 loadPurchasing();
-
-
 
 </script>
