0d0e37e2862ff985b0b81f0bf4c3f55a1acda292
Javas_Axie <javasaxie@gmail.com>
2026-07-07 19:26:52 +0700

    Update dana.js

--- js/dana.js
+++ js/dana.js
@@ -1,198 +1,96 @@
 <script>
 
-
 let danaModal;
-
 
 function loadDana(){
 
-
-danaModal =
-new bootstrap.Modal(
-document.getElementById(
-"danaModal"
-));
-
-
-loadDanaTable();
-
+  danaModal = new bootstrap.Modal(document.getElementById("danaModal"));
+  loadDanaTable();
 
 }
-
-
 
 function loadDanaTable(){
 
+  google.script.run
 
-google.script.run
+  .withSuccessHandler(function(data){
 
-.withSuccessHandler(function(data){
+    let html="";
 
+    data.forEach(x=>{
 
-let html="";
+      html+=`
+      <tr>
+      <td>${x.date}</td>
+      <td>${x.sumber}</td>
+      <td>${x.tujuan}</td>
+      <td>${formatMoney(x.nominal)}</td>
+      <td>${x.via}</td>
+      <td><button class="btn btn-danger btn-sm" onclick="deleteDana('${x.id}')"><i class="bi bi-trash"></i></button></td>
+      </tr>
+      `;
 
+    });
 
-data.forEach(x=>{
+    danaTable.innerHTML=html;
 
+  })
 
-html+=`
-
-<tr>
-
-<td>${x.date}</td>
-
-<td>${x.sumber}</td>
-
-<td>${x.tujuan}</td>
-
-<td>
-${formatMoney(x.nominal)}
-</td>
-
-
-<td>${x.via}</td>
-
-
-<td>
-
-
-<button
-class="btn btn-danger btn-sm"
-onclick="deleteDana('${x.id}')">
-
-<i class="bi bi-trash"></i>
-
-</button>
-
-
-</td>
-
-</tr>
-
-`;
-
-
-
-});
-
-
-danaTable.innerHTML=html;
-
-
-
-})
-
-.getDana();
-
-
+  .getDana();
 
 }
-
-
-
-
 
 function openDana(){
 
-
-danaModal.show();
-
+  danaModal.show();
 
 }
-
-
-
-
 
 function saveDana(){
 
+  let data={
+    date: d_date.value,
+    sumber: d_sumber.value,
+    tujuan: d_tujuan.value,
+    nominal: d_nominal.value,
+    via: d_via.value
+  };
 
-let data={
+  google.script.run
 
+  .withSuccessHandler(()=>{
 
-date:d_date.value,
+    Swal.fire("Success", "Dana masuk tersimpan", "success");
+    danaModal.hide();
+    loadDanaTable();
 
-sumber:d_sumber.value,
+  })
 
-tujuan:d_tujuan.value,
-
-nominal:d_nominal.value,
-
-via:d_via.value
-
-
-};
-
-
-
-google.script.run
-
-.withSuccessHandler(()=>{
-
-
-Swal.fire(
-"Success",
-"Dana masuk tersimpan",
-"success"
-);
-
-
-danaModal.hide();
-
-
-loadDanaTable();
-
-
-})
-
-
-.saveDana(data);
-
-
+  .saveDana(data);
 
 }
-
-
-
-
 
 function deleteDana(id){
 
+  google.script.run
 
-google.script.run
+  .withSuccessHandler(()=>{
+    loadDanaTable();
+  })
 
-.withSuccessHandler(()=>{
-
-loadDanaTable();
-
-})
-
-.deleteDana(id);
-
-
+  .deleteDana(id);
 
 }
-
-
-
-
 
 function formatMoney(v){
 
-return new Intl.NumberFormat(
-"id-ID",
-{
-style:"currency",
-currency:"IDR"
-}
-).format(v);
-
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR"
+  }).format(v);
 
 }
-
-
 
 loadDana();
-
 
 </script>
